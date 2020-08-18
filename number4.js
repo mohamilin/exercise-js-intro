0b0e9abe3940e9a3234226abae9b85f682105978
mohamilin <amilinmoh99@gmail.com>
2020-08-18 10:27:06 +0700

    solved number 4

--- number4.js
+++ number4.js
@@ -8,9 +8,13 @@
 
 /*
 
-prompt("Masukkan Nama User");
-prompt("Masukkan Asal Negara");
-confirm("Apakah data yang diisi sudah benar ? ");
-alert("Terima kasih sudah mengisi form");
+let userName = prompt("Masukkan Nama User");
+let userCity = prompt("Masukkan Asal Negara");
+let userCheck = confirm("Apakah data yang diisi sudah benar ? ");
+if (userCheck == true) {
+  alert("Terima kasih sudah mengisi form");
+} else {
+  alert("Anda membatalkan pengisian");
+}
 
 */
